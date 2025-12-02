bfdaaef51b43a4faf8ebe64862bbf6b924bf8583
Balaji <poralabalaji@gmail.com>
2025-12-02 15:33:18 +0530

    Data Validation Completed

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -5,3 +5,13 @@ class DataIngestionArtifact:
     """Data class to represent the artifact of data ingestion process."""
     training_file_path: str
     testing_file_path: str
+
+@dataclass
+class DataValidationArtifact:
+    """Data class to represent the artifact of data validation process."""
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
